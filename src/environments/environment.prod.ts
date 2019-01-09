07355a482e433a199c7058812d2882b408ea20d0
Robin Lieson <robin.lieson@appschef.com>
2019-01-09 11:21:41 +0700

    fix webpack, tslint, and enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaxlriQ0VwcpGGogsNLj7szky6MjfNbGU",
+    authDomain: "angularupload-74b9f.firebaseapp.com",
+    databaseURL: "https://angularupload-74b9f.firebaseio.com",
+    projectId: "angularupload-74b9f",
+    storageBucket: "angularupload-74b9f.appspot.com",
+    messagingSenderId: "116655928898"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBaxlriQ0VwcpGGogsNLj7szky6MjfNbGU',
-    authDomain: 'angularupload-74b9f.firebaseapp.com',
-    databaseURL: 'https://angularupload-74b9f.firebaseio.com',
-    projectId: 'angularupload-74b9f',
-    storageBucket: 'angularupload-74b9f.appspot.com',
-    messagingSenderId: '116655928898'
+    apiKey: "AIzaSyBaxlriQ0VwcpGGogsNLj7szky6MjfNbGU",
+    authDomain: "angularupload-74b9f.firebaseapp.com",
+    databaseURL: "https://angularupload-74b9f.firebaseio.com",
+    projectId: "angularupload-74b9f",
+    storageBucket: "angularupload-74b9f.appspot.com",
+    messagingSenderId: "116655928898"
   }
 };
 
